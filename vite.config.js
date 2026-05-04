fadff27da9c0ecf62e1da996f023c25895645efd
J-Henriksson <henriksson.joel2@gmail.com>
2026-05-04 18:28:31 +0200

    Add second page to deployment + improve header bar

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/femton-noll-tre/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        visningar: resolve(__dirname, 'visningar.html'),
+      },
+    },
+  },
 })
