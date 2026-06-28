94600f57a30e5b51152a52df044cd18e1fee91ee
J-Henriksson <henriksson.joel2@gmail.com>
2026-06-29 01:04:14 +0200

    Refactor layout

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         visningar: resolve(__dirname, 'visningar.html'),
+        merInfo: resolve(__dirname, 'mer-info.html'),
       },
     },
   },
